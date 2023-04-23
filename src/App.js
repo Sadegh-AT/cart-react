9b05723090e854bbac5a949a11b1878952dff9d3
SadeghAT <sadegh.artivox@gmail.com>
2023-04-23 17:14:02 +0330

    fix image src

--- src/App.js
+++ src/App.js
@@ -18,69 +18,72 @@ export default class App extends Component {
         {
           id: 1,
           image:
-            "./image/airplane_architecture_bottom_view_126215_3840x2160.jpg",
+            "./image/airplane_architecture_bottom_view_126215_3840x2160-min.jpg",
           price: "200",
         },
         {
           id: 2,
-          image: "./image/arch_temple_architecture_126579_3840x2160.jpg",
+          image: "./image/arch_temple_architecture_126579_3840x2160-min.jpg",
           price: "610",
         },
         {
           id: 3,
-          image: "./image/bridge_night_city_lighting_125945_3840x2160.jpg",
+          image: "./image/bridge_night_city_lighting_125945_3840x2160-min.jpg",
           price: "530",
         },
         {
           id: 4,
-          image: "./image/eiffel_tower_paris_night_city_126429_3840x2160.jpg",
+          image:
+            "./image/eiffel_tower_paris_night_city_126429_3840x2160-min.jpg",
           price: "351",
         },
         {
           id: 5,
-          image: "./image/facade_architecture_skyscrapers_126659_3840x2160.jpg",
+          image:
+            "./image/facade_architecture_skyscrapers_126659_3840x2160-min.jpg",
           price: "522",
         },
         {
           id: 6,
-          image: "./image/ferris_wheel_night_city_london_126187_3840x2160.jpg",
+          image:
+            "./image/ferris_wheel_night_city_london_126187_3840x2160-min.jpg",
           price: "710",
         },
         {
           id: 7,
-          image: "./image/london_eye.jpg",
+          image: "./image/london_eye-min.jpg",
           price: "407",
         },
         {
           id: 8,
-          image: "./image/mountain_8-.jpg",
+          image: "./image/mountain_8--min.jpg",
           price: "610",
         },
         {
           id: 9,
-          image: "./image/new_highs_in_the_sky-.jpg",
+          image: "./image/new_highs_in_the_sky--min.jpg",
           price: "232",
         },
         {
           id: 10,
-          image: "./image/new_york_city_night_lights.jpg",
+          image: "./image/new_york_city_night_lights-min.jpg",
           price: "131",
         },
         {
           id: 11,
           image:
-            "./image/skyscrapers_panorama_city_lights_126505_3840x2160.jpg",
+            "./image/skyscrapers_panorama_city_lights_126505_3840x2160-min.jpg",
           price: "431",
         },
         {
           id: 12,
           image:
-            "./image/sydney_opera_house_theater_harbor_126385_3840x2160.jpg",
+            "./image/sydney_opera_house_theater_harbor_126385_3840x2160-min.jpg",
           price: "270",
         },
         {
           id: 13,
-          image: "./image/tianjin-2185510_1920.jpg",
+          image: "./image/tianjin-2185510_1920-min.jpg",
           price: "489",
         },
       ],
