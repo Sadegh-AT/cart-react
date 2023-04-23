facfdb1836f25379e4fa072560f783d50b4f6ec3
SadeghAT <sadegh.artivox@gmail.com>
2023-04-23 13:56:38 +0330

    make component structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+// Product
+import Product from "./Components/Product/Product";
+
+// Cart
+import Cart from "./Components/Cart/Cart";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Product></Product>
+        <Cart></Cart>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Product.css";
+
+export default class Product extends Component {
+  render() {
+    return (
+      <div className="Product">
+        <div className="product-container"></div>
+      </div>
+    );
+  }
+}
